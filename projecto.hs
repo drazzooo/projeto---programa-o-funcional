edbb5f6d122b7fc6317b5b463153c0c9a0b579ad
Ruan Vieira <ruanvieira.pimentel@gmail.com>
2026-04-09 19:11:54 +0100

    Update comments and fix formatting in projecto.hs

--- projecto.hs
+++ projecto.hs
@@ -1,20 +1,27 @@
+-- Autores: 
+-- Ruan Vieira Damasceno Pimentel - 66502
+-- Alexandre Felli Chimello - 66474
+
+-- Exemplo: sigmoid 1 == 0.7310585786300049
 sigmoid :: Double -> Double 
 sigmoid x = 1 / (1 + exp (-x))
 
+-- Exemplo: sigmoid' 0.75 == 0.1875
 sigmoid' :: Double -> Double
 sigmoid' x = x * (1 - x)
 
+-- Exemplo: somaVectorial [33,25] [17,15] == [50,40]
 somaVectorial :: [Double] -> [Double] -> [Double]
 somaVectorial xs ys = zipWith (+) xs ys
 
--- Exemplo: 3 [2, 4, 6, 8] == [[2, 4, 6], [8]]
+-- Exemplo: chunksOf 3 [2, 4, 6, 8] == [[2, 4, 6], [8]]
 chunksOf :: Int -> [a] -> [[a]]
 chunksOf _ [] = []
 chunksOf n list
     | n <= 0 = error "O tamanho do grupo deve ser maior que zero"
     | otherwise = take n list : chunksOf n (drop n list)
 
--- Exemplo: [[4, 8], [3, 6]] == [[4, 3],[8, 6]]
+-- Exemplo: transpose [[4, 8], [3, 6]] == [[4, 3],[8, 6]]
 transpose :: [[a]] -> [[a]]
 transpose [] = []
 transpose ([] : _) = []
