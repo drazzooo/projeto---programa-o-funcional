9cd82658fc3bf6b8f41c3329e4f1d1f0e2336eae
AlexandreFelliChimello <aleirado@Outlook.com>
2026-04-09 16:14:17 +0100

    Update projecto.hs

--- projecto.hs
+++ projecto.hs
@@ -6,3 +6,14 @@ sigmoid' x = x * (1 - x)
 
 somaVectorial :: [Double] -> [Double] -> [Double]
 somaVectorial xs ys = zipWith (+) xs ys
+
+chunksOf :: Int -> [a] -> [[a]]
+chunksOf _ [] = []
+chunksOf n list
+    | n <= 0 = error "O tamanho do grupo deve ser maior que zero"
+    | otherwise = take n list : chunksOf n (drop n list)
+
+transpose' :: [[a]] -> [[a]]
+transpose' [] = []
+transpose' ([] : _) = []
+transpose' matrix = map head matrix : transpose' (map tail matrix)
